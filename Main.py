4a50ea785501b3782ea6be3e6dc76f5a26b90a59
MarsvsDunya <arifmammedov2006@gmail.com>
2026-08-10 12:34:23 +0300

    optimized functions

--- Main.py
+++ Main.py
@@ -4,7 +4,6 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
-
 data = load_breast_cancer()
 X = data.data
 Y = data.target
@@ -13,8 +12,8 @@ X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size = 0.2,random_state=
 
 model = MyTree()
 model.fit(X_train,Y_train)
-print(1)
 
 y_pred = model.predict(X_test)
-print("Accuracy:",model.calculateAcc(y_pred,Y_test))
+print("Accuracy:",model.calculateAcc(Y_test,y_pred))
+
 print(classification_report(y_pred,Y_test))
--- decision_tree.py
+++ decision_tree.py
@@ -3,41 +3,46 @@ from Node import Node
 
 class MyTree:
 
-    thresholds=[]
     def __init__(self, max_depth=10, min_samples_split=10,min_gain=0.01):
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
         self.min_gain = min_gain
         self.root = None
+        self.thresholds = None
+
 
     def fillThreshold(self, X, Y, method):
         m = X.shape[1]
-        self.thresholds = np.empty(m,dtype=int)
+        self.thresholds = np.empty(m,dtype=float)
         for i in range(m):
             bestValue = MyTree.best_Value(X, Y, i, method)
             self.thresholds[i] = bestValue
 
+
     @staticmethod
     def compute_impurity(Y):
         impurity = .0
-        if len(Y) == 0: return 0
+        if (len(Y) == 0): return 0
         if (len(np.unique(Y)) == 1): return 0
 
-        p1 = len(Y[Y == 1]) / len(Y)
+        p1 = np.mean(Y)
         p2 = 1 - p1
+
         impurity = np.log2(p1) * (-p1) - np.log2(p2) * p2
         return impurity
 
     @staticmethod
     def best_Value(X, Y, feature_no, method):
         values = method(X[:, feature_no])
-        maxGain = 0
-        bestValue = -1
+        maxGain = -np.inf
+        bestValue = None
         for i in values:
             gain = MyTree.info_gain(X, Y, feature_no, i)
-            if (gain[0] > maxGain):
-                maxGain =gain[0]
+
+            if gain > maxGain:
+                maxGain = gain
                 bestValue = i
+
         return bestValue
 
     @staticmethod
@@ -49,7 +54,7 @@ class MyTree:
     @staticmethod
     def quintile_threshold(X):
         X_sorted = np.sort(X)
-        quintiles = np.linspace(0, 100, 10)[1:-1]
+        quintiles = np.linspace(0, 100, 20)[1:-1]
         values = np.ones(len(quintiles))
         a = 0
         for i in quintiles:
@@ -65,28 +70,35 @@ class MyTree:
         return thresholds
 
     @staticmethod
-    def info_gain(X, Y, feature, threshold):
+    def info_gain(X, Y, feature_no, threshold):
         if len(Y) <= 1: return 0
         base_impurity = MyTree.compute_impurity(Y)
-        feature_values = X[:, feature]
+        feature_values = X[:, feature_no]
 
         mask_left = feature_values <= threshold
-        mask_right = feature_values > threshold
+        mask_right = ~mask_left
 
-        X_left = X[mask_left]
-        X_right = X[mask_right]
         Y_left = Y[mask_left]
         Y_right = Y[mask_right]
+        n = len(Y)
 
         p_left = MyTree.compute_impurity(Y_left)
         p_right = MyTree.compute_impurity(Y_right)
 
-        w_left = len(Y_left) / len(Y)
-        w_right = len(Y_right) / len(Y)
+        w_left = len(Y_left) / n
+        w_right = len(Y_right) / n
 
         gain = base_impurity - (p_left * w_left + p_right * w_right)
 
-        return [gain, X_left, X_right, Y_left, Y_right]
+        return gain
+
+    @staticmethod
+    def _splitData(X, Y, feature_no, threshold):
+        mask = X[:, feature_no] <= threshold
+
+        return [X[mask], X[~mask], Y[mask], Y[~mask]]
+
+
 
     def splitTree(self,node, X, Y, dept):
         if (node is None): node = Node()
@@ -105,33 +117,37 @@ class MyTree:
             node.value = np.bincount(Y).argmax()
             return node
 
-        feature = None
-        bestGain = [0]
+        bestFeature = None
+        bestGain = 0
+        bestThreshold = None
 
         for i in range(len(self.thresholds)):
             gain = MyTree.info_gain(X, Y, i, self.thresholds[i])
-            if (bestGain[0] < gain[0]):
+            if bestGain < gain:
                 bestGain = gain
-                feature = i
+                bestFeature = i
+                bestThreshold = self.thresholds[i]
 
-        if (bestGain[0] < self.min_gain):
+        if bestGain < self.min_gain:
             node.value = np.bincount(Y).argmax()
             return node
 
-        node.feature = feature
-        node.threshold = self.thresholds[feature]
+        node.feature = bestFeature
+        node.threshold = bestThreshold
         node.dept = dept
+
+        X_left, X_right, Y_left, Y_right = MyTree._splitData(X, Y, bestFeature, bestThreshold)
 
         print(
             "Depth:", node.dept,
             "Samples:", len(Y),
             "Feature:", node.feature,
-            "Gain:", "{:.4f}".format(bestGain[0]),
-            "Threshold:", "{:.2f}".format(node.threshold)
+            "Gain:", "{:.4f}".format(bestGain),
+            "Threshold:", node.threshold
         )
 
-        node.left = self.splitTree(Node(), bestGain[1], bestGain[3], dept + 1)
-        node.right = self.splitTree(Node(), bestGain[2], bestGain[4], dept + 1)
+        node.left = self.splitTree(Node(), X_left, Y_left, dept + 1)
+        node.right = self.splitTree(Node(), X_right, Y_right, dept + 1)
 
         return node
 
@@ -165,9 +181,6 @@ class MyTree:
 
         self._predict_recursive(node.left, X, left, prediction)
         self._predict_recursive(node.right, X, right, prediction)
-
-
-
 
     def calculateAcc(self,y_pre, y):
         return np.mean(y_pre == y)
